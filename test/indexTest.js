e90ae8162f341b6e85c87f805fab8bb65364f12b
Josh French <joshfrench@gmail.com>
2016-08-20 14:14:23 -0700

    Legibility

--- test/indexTest.js
+++ test/indexTest.js
@@ -2,13 +2,14 @@ import { expect } from 'chai';
 import markersToTree from '../src/index';
 
 const tree = ['P', []];
+let result;
 
 describe('markersToTree()', () => {
   it('renders a simple marker', () => {
     const markers = [
       [[], 0, 'No children']
     ];
-    const [result] = markers.reduce(markersToTree, [tree]);
+    [result] = markers.reduce(markersToTree, [tree]);
     expect(result).to.eql(['P', ['No children']]);
   });
 
@@ -25,7 +26,46 @@ describe('markersToTree()', () => {
       [[], 0, 'A'],
       [[], 0, 'B']
     ];
-    const [result] = markers.reduce(markersToTree, [tree]);
+    [result] = markers.reduce(markersToTree, [tree]);
     expect(result).to.eql(['P', ['A', 'B']]);
+  });
+
+  it('renders simple nested marker', () => {
+    const markers = [
+      [['A'], 0, 'Opens A'],
+      [[], 1, 'Closes A']
+    ];
+    [result] = markers.reduce(markersToTree, [tree]);
+    expect(result).to.eql(
+      ['P', [
+        ['A', ['Opens A', 'Closes A']]]]);
+  });
+
+  it('renders multiple nested markers', () => {
+    const markers = [
+      [['A'], 0, 'Opens A'],
+      [['B'], 2, 'Opens B, Closes A and B']
+    ];
+    [result] = markers.reduce(markersToTree, [tree]);
+    expect(result).to.eql(
+      ['P', [
+        ['A', [
+          'Opens A',
+          ['B', ['Opens B, Closes A and B']]]]]]);
+  });
+
+  it('closes intermediary tags', () => {
+    const markers = [
+      [['A'], 0, 'Opens A'],
+      [['B'], 1, 'Opens/closes B'],
+      [[], 1, 'Closes A']
+    ];
+    [result] = markers.reduce(markersToTree, [tree]);
+    expect(result).to.eql(
+      ['P', [
+        ['A', [
+          'Opens A',
+          ['B', ['Opens/closes B']],
+          'Closes A']]]]);
   });
 });
