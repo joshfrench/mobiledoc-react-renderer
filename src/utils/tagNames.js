4d91f20f6a9a2ade6b37d1dae2f27dcd3120e35c
Josh French <joshfrench@gmail.com>
2016-08-26 22:02:18 -0700

    Add missing helper

---
+++ src/utils/tagNames.js
@@ -0,0 +1,41 @@
+import { MARKUP_SECTION_TYPE } from './nodeTypes';
+
+const normalizeTagName = (tag) => tag.toLowerCase();
+
+const MARKUP_SECTION_TAG_NAMES = [
+  'p', 'h1', 'h2', 'h3', 'blockquote', 'pull-quote'
+].map(normalizeTagName);
+
+const MARKUP_SECTION_ELEMENT_NAMES = [
+  'p', 'h1', 'h2', 'h3', 'blockquote'
+].map(normalizeTagName);
+
+// const LIST_SECTION_TAG_NAMES = [
+//   'ul', 'ol'
+// ].map(normalizeTagName);
+//
+// const MARKUP_TYPES = [
+//   'b', 'i', 'strong', 'em', 'a', 'u', 'sub', 'sup', 's'
+// ].map(normalizeTagName);
+
+function contains(array, item) {
+  return array.indexOf(item) !== -1;
+}
+
+export function isValidSectionTagName(tagName, sectionType) {
+  tagName = normalizeTagName(tagName);
+
+  switch (sectionType) {
+  case MARKUP_SECTION_TYPE:
+    return contains(MARKUP_SECTION_TAG_NAMES, tagName);
+  // case LIST_SECTION_TYPE:
+  //   return contains(LIST_SECTION_TAG_NAMES, tagName);
+  default:
+    throw new Error(`Cannot validate tagName for unknown section type "${sectionType}"`);
+  }
+}
+
+export function isMarkupSectionElementName(tagName) {
+  tagName = normalizeTagName(tagName);
+  return contains(MARKUP_SECTION_ELEMENT_NAMES, tagName);
+}
