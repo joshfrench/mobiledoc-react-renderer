f24d4cdb7958505c1a8970954a0a3e291b582a46
Josh French <joshfrench@gmail.com>
2017-12-03 17:23:15 -0800

    Wire up unknownCardHandler and unknownAtomHandler

--- src/MobiledocRenderer.js
+++ src/MobiledocRenderer.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { sectionToTree } from './Section';
 import { nodesToTags } from './Tree';
-import { treeToReact } from './ReactRenderer';
+import { nodeToComponent } from './ReactRenderer';
 
-const MobiledocRenderer = ({ mobiledoc = {}, rootElement = 'div', sectionElementRenderer, markupElementRenderer, atoms, cards, ...props }) => {
+const MobiledocRenderer = ({ mobiledoc = {}, rootElement = 'div',
+                             sectionElementRenderer, markupElementRenderer,
+                             atoms = [], cards = [],
+                             unknownCardHandler, unknownAtomHandler,
+                             ...props }) => {
   mobiledoc.sections = mobiledoc.sections || [];
   const children = mobiledoc.sections.map(sectionToTree)
                                      .map(nodesToTags(mobiledoc))
-                                     .map(treeToReact({ sectionElementRenderer, markupElementRenderer, cards, atoms }));
+                                     .map(nodeToComponent({ cards, atoms,
+                                                            sectionElementRenderer, markupElementRenderer,
+                                                            unknownCardHandler, unknownAtomHandler }));
 
   return React.createElement(rootElement, props, children);
 };
